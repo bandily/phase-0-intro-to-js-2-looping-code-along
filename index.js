a9b9ebb2b8f786e7d3cee0c41e4e448fa813e958
Brian <huynh.sbrian@gmail.com>
2022-07-05 10:04:46 -0700

    commit tree 9078f6b8048f77d1c9ef997d0bdab787323121ae

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards() {
+    const name = ["Thank you, Guadalupe, for the wonderful surprise gift!", "Thank you, Ollie, for the wonderful surprise gift!", "Thank you, Aki, for the wonderful surprise gift!"];
+    return name;
+}
+
+writeCards();
+
+function countDown() {
+    let coundDown = 10;
+    while (coundDown >= 0) {
+        console.log(coundDown--);
+    }
+}
